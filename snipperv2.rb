f5c122e398224e483a166fa35a5523920abf08c0
hatlord <rich@Computar.local>
2016-11-17 13:48:27 +0000

    Added checkpoint support and wrote the CSV writer POC

--- snipperv2.rb
+++ snipperv2.rb
@@ -11,13 +11,13 @@ class Parsexml
   attr_reader :netw_srvc, :audit_rec
 
   def initialize
-    @fwpol = Nokogiri::XML(File.read(ARGV[0]))
+    @fwpol      = Nokogiri::XML(File.read(ARGV[0]))
     @rule_array = []
     @vuln_array = []
     @user_array = []
     @netw_srvc  = []
     @audit_rec  = []
-    @device = {}
+    @device     = {}
   end
 
   def device_type
@@ -108,7 +108,7 @@ class Parsexml
   end
 
   def cisco
-    if @device[:type] =~ /Cisco Adaptive Security Appliance Firewall/i
+    if @device[:type] =~ /Cisco/i
       @fwpol.xpath('//document/report/part/section').each do |title|
         rules = {}
         rules[:title]  = title.xpath('@title').text
@@ -123,12 +123,12 @@ class Parsexml
                 rules[:name]   = item.xpath('./tablecell[1]/item').text
                 rules[:active] = item.xpath('./tablecell[2]/item').text
                 rules[:action] = item.xpath('./tablecell[3]/item').text
-                rules[:proto]  = item.xpath('./tablecell[4]/item').map(&:text)
-                rules[:src]    = item.xpath('./tablecell[5]/item').map(&:text)
-                rules[:srcprt] = item.xpath('./tablecell[6]/item').map(&:text)
-                rules[:dst]    = item.xpath('./tablecell[7]/item').map(&:text)
-                rules[:dport]  = item.xpath('./tablecell[8]/item').map(&:text)
-                rules[:srvc]   = item.xpath('./tablecell[9]/item').map(&:text)
+                rules[:proto]  = item.xpath('./tablecell[4]/item').map(&:text).join("\r")
+                rules[:src]    = item.xpath('./tablecell[5]/item').map(&:text).join("\r")
+                rules[:srcprt] = item.xpath('./tablecell[6]/item').map(&:text).join("\r")
+                rules[:dst]    = item.xpath('./tablecell[7]/item').map(&:text).join("\r")
+                rules[:dport]  = item.xpath('./tablecell[8]/item').map(&:text).join("\r")
+                rules[:srvc]   = item.xpath('./tablecell[9]/item').map(&:text).join("\r")
                 rules[:log]    = item.xpath('./tablecell[10]/item').text
 
                 @rule_array << rules.dup
@@ -141,9 +141,40 @@ class Parsexml
     # @rule_array.each { |r| puts "#{r[:title]},#{r[:table]},#{r[:name]},#{r[:active]},#{r[:action]},#{r[:src]},#{r[:srcprt]},#{r[:dst]},#{r[:dport]},#{r[:srvc]},#{r[:log]}"}
   end
 
+  def checkpoint
+    if @device[:type] =~ /Checkpoint|Alteon/
+    @fwpol.xpath('//document/report/part/section').each do |title|
+        rules = {}
+        rules[:title]  = title.xpath('@title').text
+
+        title.xpath('./section/table').each do |info|
+          rules[:table]    = info.xpath('@title').text
+          rules[:ref]      = info.xpath('@ref').text
+
+            info.xpath('./tablebody/tablerow').each do |item|
+              if rules[:ref] =~ /FILTER\./
+                rules[:name]      = item.xpath('./tablecell[1]/item').text
+                rules[:active]    = item.xpath('./tablecell[2]/item').text
+                rules[:action]    = item.xpath('./tablecell[3]/item').text
+                rules[:src]       = item.xpath('./tablecell[4]/item').map(&:text).join("\r")
+                rules[:dst]       = item.xpath('./tablecell[5]/item').map(&:text).join("\r")
+                rules[:srvc]      = item.xpath('./tablecell[6]/item').map(&:text).join("\r")
+                rules[:time]      = item.xpath('./tablecell[7]/item').map(&:text).join("\r")
+                rules[:install]   = item.xpath('./tablecell[8]/item').map(&:text).join("\r")
+                rules[:through]   = item.xpath('./tablecell[9]/item').map(&:text).join("\r")
+                rules[:log]       = item.xpath('./tablecell[10]/item').map(&:text).join("\r")
+
+                @rule_array << rules.dup 
+
+            end
+          end
+        end
+      end
+    end
+  end
 
   def other
-    if @device[:type] !~ /Cisco Adaptive Security Appliance Firewall/i
+    if @device[:type] !~ /Cisco|Alteon|Checkpoint/i
       @fwpol.xpath('//document/report/part/section').each do |title|
         rules = {}
         rules[:title]  = title.xpath('@title').text
@@ -156,10 +187,10 @@ class Parsexml
               if rules[:ref] =~ /FILTER\./
                 rules[:name]   = item.xpath('./tablecell[1]/item').text
                 rules[:action] = item.xpath('./tablecell[2]/item').text
-                rules[:src]    = item.xpath('./tablecell[3]/item').map(&:text)
-                rules[:dst]    = item.xpath('./tablecell[4]/item').map(&:text)
-                rules[:srvc]   = item.xpath('./tablecell[5]/item').map(&:text)
-                rules[:log]    = item.xpath('./tablecell[6]/item').text
+                rules[:src]    = item.xpath('./tablecell[3]/item').map(&:text).join("\r")
+                rules[:dst]    = item.xpath('./tablecell[4]/item').map(&:text).join("\r")
+                rules[:srvc]   = item.xpath('./tablecell[5]/item').map(&:text).join("\r")
+                rules[:log]    = item.xpath('./tablecell[6]/item').map(&:text).join("\r")
 
                 @rule_array << rules.dup 
 
@@ -172,20 +203,19 @@ class Parsexml
   end
 
   def rules
-    #don't like this, find a better way?
     @rule_array
   end
 
 end
 
-class Sort_data
+class Output
 
   def initialize(fwparse)
     @fwparse = fwparse
-    # @admin = []
   end
 
   def build_arrays
+    #may need to break these out into indivudual methods or i won't be able to access the arrays!!
 
     @adminsrv        = @fwparse.rules.select { |r| r[:title] =~ /Allow Access To Administrative Services/ }
     @plaintext       = @fwparse.rules.select { |r| r[:title] =~ /Access To Clear-Text Protocol/ }
@@ -194,39 +224,36 @@ class Sort_data
     @sensitive       = @fwparse.rules.select { |r| r[:title] =~ /Potentially Sensitive Services/ }
     @nologging       = @fwparse.rules.select { |r| r[:title] =~ /Configured Without Logging/ }
     @legacy          = @fwparse.rules.select { |r| r[:title] =~ /Potentially Unnecessary Services/ }
-    @norules         = @fwparse.rules.select { |r| r[:title] =~ /No Network Filtering Rules Were Configured/ } #Need to find a config that this will work on
-    # puts @fwparse.vuln_array
-    # @fwparse.rules.each { |r| puts r[:title]}
-    # puts @fwparse.rules
-    # puts @over_permissive
-    puts @permitall
-    # puts @permit
-  end
-
-  def afunction
+    @norules         = @fwparse.rules.select { |r| r[:title] =~ /No Network Filtering Rules Were Configured/ } #Need to find a config that this will work on (New SYSTEM/ADMIN)
 
   end
 
-end
-
-#This class will deal with output to other classes
-class Output
-
-  def initialize(fwparse)
-    @fwparse = fwparse
+  def createcsv
+    if @fwparse.rules
+      Dir.mkdir("#{Dir.home}/Documents/Snipper_Out/") unless File.exists?("#{Dir.home}/Documents/Snipper_Out/")
+      @test = File.new("#{Dir.home}/Documents/Snipper_Out/testing.csv", 'w+')
+    end
   end
 
-  def printme
-    #This works but needs to change given that we aren't printing much/anything to console
-    puts "#{@fwparse.device[:name]}\t#{@fwparse.device[:type]}\t#{@fwparse.device[:os]}\t#{@fwparse.device[:version]}".red.bold
+  def generate_data
+    adminstring = CSV.generate do |csv|
+      csv << @adminsrv.first.keys if !@adminsrv.empty? #Avoids nil errors when creating.
+      @adminsrv.each { |row| csv << row.values }
+    end
+    @test.puts(adminstring)
+    @test.close
   end
 
 end
+
+#This class will deal with output to other classes/files/console
+
 
 
 fwparse = Parsexml.new
 fwparse.device_type
 fwparse.cisco
+fwparse.checkpoint
 fwparse.other
 fwparse.users
 fwparse.net_services
@@ -234,9 +261,11 @@ fwparse.auditrec
 fwparse.vulns
 
 
-sortme = Sort_data.new(fwparse)
-sortme.build_arrays
-sortme.afunction
+output = Output.new(fwparse)
+output.build_arrays
+output.createcsv
+output.generate_data
+
 
 # printer = Output.new(fwparse)
 # printer.printme
